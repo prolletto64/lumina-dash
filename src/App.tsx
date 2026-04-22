ecfebcd47e53ee3edf5b1ee226c2b6226f2212dc
prolletto64 <github@prolletto64.eu>
2026-04-22 11:09:19 +0200

    Random user loading
    Signed-off-by: prolletto64 <github@prolletto64.eu>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import './App.css'
+import {useEffect, useState} from "react";
 
 function App() {
-    return (
-        <>
+    const [name, setName] = useState('')
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+    useEffect(() => {
+        const fetchRandomUser = async () => {
+            try {
+                const response = await fetch('https://randomuser.me/api/', {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+                if (!response.ok) throw new Error('Errore nella chiamata API');
+                const data = await response.json();
+                const user = data.results[0];
+                setName(user.name.first);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchRandomUser();
+    }, [])
 
-        </>
+    return (
+        <div className="dashboard">
+            {isLoading && <div className="centered-text"><p className="text-primary">LumaDash is loading...</p></div>}
+            {!isLoading && <div>
+                <h1 className="title">Luma Dashboard</h1>
+                <p className="text-primary">Welcome, {name}</p>
+            </div>}
+        </div>
     )
 }
 
