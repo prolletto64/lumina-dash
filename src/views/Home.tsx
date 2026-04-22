6ed4d1071f7391a3b56458bcd193fc41b01c2d96
prolletto64 <github@prolletto64.eu>
2026-04-22 11:29:32 +0200

    added header
    Signed-off-by: prolletto64 <github@prolletto64.eu>

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -5,10 +5,20 @@ interface HomeProps {
 export function Home({name}: HomeProps) {
     return (
         <>
-            <div>
+            <header className="header">
+                <div>
                 <h1 className="title">Luma Dashboard</h1>
                 <p className="text-primary">Welcome, {name}</p>
-            </div>
+                </div>
+                <div>
+                    <h1 className="title">{new Date().toLocaleDateString('en-US', {
+                        month: 'long',
+                        day: 'numeric',
+                        year: 'numeric'
+                    })}</h1>
+                </div>
+            </header>
+
         </>
     )
 }
